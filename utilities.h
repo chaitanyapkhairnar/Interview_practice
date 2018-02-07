56395673f10a68cec5dd3a6d353f8498ceafd2b6
Chaitanya Khairnar <chaitanyapkhairnar@gmail.com>
2018-02-07 14:46:15 -0800

    Adding basic utility header file. Testing first commit into this new repository.

---
+++ utilities.h
@@ -0,0 +1,12 @@
+/*
+ * This is the header file containing all the
+ * structure and function definitions
+ */
+
+/*
+ * Node structure for Linked List
+ */
+typedef struct ll_node {
+    int value;
+    ll_node *next;
+}ll_node;

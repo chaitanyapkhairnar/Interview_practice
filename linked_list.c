5ebe04abbf4b60e8e7d89b0caccd02ccd1f66479
Chaitanya Khairnar <chaitanyapkhairnar@gmail.com>
2018-02-07 20:47:44 -0800

    Added a new file for linked list related operations.

---
+++ linked_list.c
@@ -0,0 +1,90 @@
+/*
+ *
+ * Author: Chaitanya
+ *
+ * File: linked_list.c
+ *
+ * Info: This file contains all the operations
+ *       related to linked lists. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utilities.h"
+
+/*
+ * Info: Function to create a new linked list node
+ * with the given value.
+ *
+ * Arguments: val - Value field of the new node
+ *
+ * Return type: ll_node - Newly created node
+ */
+ll_node* ll_add_node (int val) {
+    ll_node *new_node = NULL;
+
+    new_node = (ll_node*)malloc(sizeof(ll_node));
+    if(!new_node) {
+        printf("Failed to allocate memory to ll_node structure. Returning");
+        return NULL;
+    }
+    new_node->value = val;
+    new_node->next = NULL;
+    return new_node;
+}
+
+/*
+ * Info: Function to free memory of the given node.
+ *
+ * Arguments: node - ll_node type node which needs
+ *                   to be freed.
+ *
+ * Return type: int - 0 if successfull -1 if error.
+ */
+int ll_free_node (ll_node *node) {
+    if (!node) {
+        printf("Given node does not exists. Returning error.");
+        return -1;
+    }
+    free(node);
+    node = NULL;
+    return 0;
+}
+
+/*
+ * Info: Function to insert a linked list node
+ *       to the given list at given position.
+ *
+ * Arguments: head - ll_node type pointer to head of
+ *                   list.
+ *            pos - integer type variable for position.
+ *            val - integer type value for new node.
+ *
+ * Return type: ll_node - Head node of the list.
+ */
+ll_node* ll_insert_node (ll_node *head, int pos, int val) {
+    ll_node *new_node = NULL;
+
+    if (!head) {
+        printf("Head node is NULL. Returning.");
+        return head;
+    }
+    /* if pos is 0, that means add new node before
+     * head node and make this new head.
+     */
+    if (pos == 0) {
+        new_node = ll_add_node(val);
+        if (!new_node) {
+            printf("Error while adding new node. Returning.");
+            return head;
+        }
+        new_node->next = head;
+        head = new_node;
+        return head;
+    } else if (pos > 0) {
+        ll_node *temp = head;
+        int i = 0;
+        for (;i<pos;i++) {
+            temp = temp->next;
+        }
--- utilities.h
+++ utilities.h
@@ -1,12 +1,33 @@
 /*
- * This is the header file containing all the
- * structure and function definitions
+ *
+ * Author: Chaitanya
+ *
+ * File: utilities.h
+ *
+ * Info: This is a utility header file containing basic
+ *       structure definitions, function declarations
+ *       and MACRO definitions.
+ *
  */
 
+#ifndef _utilities.h
+#define _utilities.h
+
 /*
- * Node structure for Linked List
+ * Linked List node
  */
 typedef struct ll_node {
     int value;
     ll_node *next;
 }ll_node;
+
+/*
+ * Binary Tree node
+ */
+typedef struct bt_node {
+    int value;
+    bt_node *left;
+    bt_node *right;
+}bt_node;
+
+#endif
